6ed38cabc06b4a404e229eb2d5a05882223a4e04
Rohan138 <rapotdar@purdue.edu>
2024-01-17 01:00:08 -0500

    wip

--- examples/dreamerv3/tests/test_models.py
+++ examples/dreamerv3/tests/test_models.py
@@ -88,8 +88,8 @@ class TestWorldModel(unittest.TestCase):
             print()
         self.assertEqual(post["stoch"].numpy().shape, (B, T, 24, 24))
         self.assertEqual(post["deter"].numpy().shape, (B, T, 256))
-        self.assertEqual(context["embed"].numpy().shape, (B, T, 842))
-        self.assertEqual(context["feat"].numpy().shape, (B, T, 842))
+        self.assertEqual(context["embed"].numpy().shape, (B, T, 3072))
+        self.assertEqual(context["feat"].numpy().shape, (B, T, 832))
 
 
 class TestActorCritic(unittest.TestCase):
@@ -115,7 +115,7 @@ class TestActorCritic(unittest.TestCase):
         start = {k: v.reshape((B, T) + v.shape[1:]) for k, v in start.items()}
         feats, states, actions = actor_critic._imagine(actor_critic, **start)
         actor_critic._imagine.reset()
-        self.assertEqual(feats.numpy().shape, (H, B * T, 842))
+        self.assertEqual(feats.numpy().shape, (H, B * T, 832))
         self.assertEqual(states["stoch"].numpy().shape, (H, B * T, 24, 24))
         self.assertEqual(states["deter"].numpy().shape, (H, B * T, 256))
         self.assertEqual(actions.numpy().shape, (H, B * T, world_model.num_actions))
